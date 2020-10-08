a626bb7a3a4c41d11312692513afa7b42ff93ed2
Jeff Breig <jjbreig@gmail.com>
2020-10-08 00:06:55 -0400

    ate cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache = None):
     # Your code here
-
+    if n == 0 or n==1:
+       return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
     pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 25
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
